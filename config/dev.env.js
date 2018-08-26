55d176090975cec2f842511582f29bab91af57af
chris2012 <evilkillerice@gmail.com>
2018-08-26 21:43:46 +0800

    2nd commit

---
+++ config/dev.env.js
@@ -0,0 +1,10 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  //定義開發環境變數, 取出時用 process.env.NODE_ENV
+  NODE_ENV: '"development"',
+  APIPATH: '"https://vue-course-api.hexschool.io/api/"',
+  CUSTOMPATH: '"chris2012"'
+})
