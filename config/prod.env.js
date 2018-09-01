62fe681652f5d097d23a4d0f7a4f9fe93f1bc4dd
chris2012 <evilkillerice@gmail.com>
2018-09-01 20:18:56 +0800

    '4rd'

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,7 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  //定義開發環境變數, 取出時用 process.env.NODE_ENV
+  NODE_ENV: '"development"',
+  APIPATH: '"https://vue-course-api.hexschool.io"',
+  CUSTOMPATH: '"chris2012"'
 }
