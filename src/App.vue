a210818762169304e9a3d785680b38f6815aa8d5
chris2012 <evilkillerice@gmail.com>
2018-11-20 21:38:58 +0800

    remove ueditor
    test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app">
     <router-view></router-view>
-  </div>
+  </div>test
 </template>
 
 <script>
 export default {
-  name: 'App',
-  created() {
-  },
-}
+  name: "App",
+  created() {}
+};
 </script>
 
 
